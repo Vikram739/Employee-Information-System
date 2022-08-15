50bd68edc4b784ceba23c661e96f588baba9bcb3
Vikram Markali <vikrammarkali007@gmail.com>
2022-08-15 11:58:50 +0530

    toggling effect for login page

---
+++ script.js
@@ -0,0 +1,12 @@
+
+/* 
+    Document   : script.js     
+    Created on : 03 May, 2021, 12:11:02 PM
+    Author     : Vikram Markali
+*/
+
+document.querySelector('.img-btn').addEventListener('click', function()
+	{
+		document.querySelector('.cont').classList.toggle('s-signup')
+	}
+);
